50194e8dca44c4830a422497b647e0af095ed31b
stefan.dragomir <stefan.dragomir.000@gmail.com>
2021-07-23 20:12:13 +0300

    aded main menu file

---
+++ examples/ex2/ts2.robot
@@ -0,0 +1,18 @@
+*** Variables ***
+${v3}             55
+
+*** Test Cases ***
+tc2
+    Should Be Equal    6    7
+    Should Be Equal    8    9
+    kw2
+    kw4
+
+*** Keywords ***
+kw2
+    Should Be Equal    6    7
+    Should Be Equal    8    9
+
+kw4
+    Should Be Equal    6    7
+    Should Be Equal    8    9
---
+++ src/widgets/main_menu.py
@@ -0,0 +1,85 @@
+
+from PyQt5.QtCore          import *
+from PyQt5.QtGui           import *
+from PyQt5.QtWidgets       import * 
+from icons.icons           import SCR_GetIcon
+
+"""******************************************************************************************
+*********************************************************************************************
+******************************************************************************************"""
+class SCR_WDG_MainMenu(QMenuBar):
+
+    def __init__(self,scrib):
+
+        QMenuBar.__init__(self,scrib)
+
+        self.scrib = scrib
+
+    def populate(self):
+
+        self.populate_file()
+
+        # self.populate_search()
+
+        # self.populate_plugins()
+
+        self.populate_help()
+
+    def populate_file(self):
+
+        _menu_file    = self.addMenu("&File")
+
+        _action = _menu_file.addAction("&New")
+        _action.setIcon(SCR_GetIcon("f35b5975f6d636d7a6418bb4941edbdf89b80b55"))
+        _action.setShortcut(QKeySequence(Qt.CTRL + Qt.Key_N))
+        _action.triggered.connect(self.scrib.clbk_new)
+
+        _action = _menu_file.addAction("&Open Test Folder")        
+        _action.setIcon(SCR_GetIcon("b28971455cf45af0e2e37a9c33ca8ca01d5a660f"))
+        _action.setShortcut(QKeySequence(Qt.CTRL + Qt.Key_O))
+        _action.triggered.connect(self.scrib.clbk_load_testfolder)
+
+        _action = _menu_file.addAction("&Open Test Suite")        
+        _action.setIcon(SCR_GetIcon("8e205a227046baee2a67b75fb12c95813784c484"))
+        _action.setShortcut(QKeySequence(Qt.CTRL + Qt.Key_T))
+        _action.triggered.connect(self.scrib.clbk_load_testsuite)
+
+        _menu_file.addSeparator()
+
+        _action = _menu_file.addAction("&Save")
+        _action.setIcon(SCR_GetIcon("eb1729093812c3f38a5e4eb2714f2bde148f6eba"))
+        _action.setShortcut(QKeySequence(Qt.CTRL + Qt.Key_S))
+        _action.triggered.connect(self.scrib.clbk_save)
+
+        _menu_file.addSeparator()
+
+        _menu_file.addMenu("&Open Recent")
+
+        _menu_file.addSeparator()
+
+        _action = _menu_file.addAction("&Exit")
+        _action.setIcon(SCR_GetIcon("325eab1e2242ef8223f4b2506db6da27384f3789"))
+        _action.setShortcut(QKeySequence(Qt.CTRL + Qt.Key_Q))
+        _action.triggered.connect(self.scrib.close)
+
+    def populate_search(self):
+
+        _menu_search = self.addMenu("&Search")
+
+    def populate_plugins(self):
+
+        _menu_plugins = self.addMenu("&Plugins")
+
+    def populate_help(self):
+
+        _menu_help = self.addMenu("&Help")
+
+        _action = _menu_help.addAction("&Documentation")
+        _action.setIcon(SCR_GetIcon("edaf568e98a4c6023a1d9eb657bc737cff2ef279"))
+        _action.setShortcut(QKeySequence(Qt.Key_F1))
+
+        _action = _menu_help.addAction("&Issue or Idea")
+        _action.setIcon(SCR_GetIcon("68887170a19a6b6eb82e9a9346dd81efa8a67f4d"))
+
+        _action = _menu_help.addAction("&About")
+        _action.setIcon(SCR_GetIcon("fd2cf51bcbd304d61dbae1fdd954d4d1ec41e535"))
